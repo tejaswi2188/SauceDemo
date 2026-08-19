08ea7a502a36c7719e2ac0302edc7a6458902695
unknown <tejaswi21.gaikwad@gmail.com>
2026-08-19 13:30:42 +0530

    Modified Configuration

---
+++ playwright.config.js
@@ -0,0 +1,41 @@
+import { defineConfig, firefox } from "@playwright/test";
+
+export default defineConfig({
+
+    testDir :'./tests',
+
+
+
+
+    use:
+    {
+        headless: false
+    },
+
+    projects:[{
+
+        name : 'firefox',
+        use :
+        {
+            browserName :'firefox',
+        },
+    },
+        {
+        name :'chromium',
+        use :
+        {
+            browserName :'chromium'
+
+        },
+    },
+        {
+        name :'webkit',
+        use :
+        {
+            browserName : 'webkit'
+        },
+    }
+        
+    ]
+
+})
--- tests/SauceDemo.spec.js
+++ tests/SauceDemo.spec.js
@@ -1,10 +1,10 @@
-import {test, chromium} from '@playwright/test';
+import {test} from '@playwright/test';
 
-test("@Regression End to End flow",async() =>{
+test("@Regression End to End flow",async({page}) =>{
 
-    const browser = await chromium.launch({headless : false});
-    const context = await browser.newContext();
-    const page = await context.newPage();
+    //const browser = await chromium.launch({headless : false});
+    //const context = await browser.newContext();
+    //const page = await context.newPage();
 
 
     await page.goto("https://www.saucedemo.com/");
@@ -13,6 +13,12 @@ test("@Regression End to End flow",async() =>{
 
     await page.waitForTimeout(3000);
     await page.locator("#login-button").click();
-    await page.waitForTimeout(6000);
+    await page.waitForTimeout(3000);
+    await page.locator(".product_sort_container").selectOption({label :'Price (high to low)'});
+    await page.waitForTimeout(3000);
+    await page.locator("button[name='add-to-cart-sauce-labs-fleece-jacket']").click();
+
+    await page.locator("//span[@class='shopping_cart_badge']").click();
+    await page.waitForTimeout(3000);
 
 })
